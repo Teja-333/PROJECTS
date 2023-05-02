840c8c784c91d5881fb61eeaaf228bb6063a0c89
Teja-333 <132332081+Teja-333@users.noreply.github.com>
2023-05-02 21:41:36 +0530

    Update campus_security.py

--- A framework for campus security through surveillance video analysis with face recognition/campus_security.py
+++ A framework for campus security through surveillance video analysis with face recognition/campus_security.py
@@ -1,1 +1,96 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+# os.system('cd PROGRAMMING')
+# os.system('cd PYTHON')
+# os.system('cd PYTHON_PROJECT')
+# os.system('cd ..')
+# os.system('cd IMAGES')
+# os.system('rm .DS_Store')
+# os.system('defaults write com.apple.desktopservices DSDontWriteNetworkStores true')
+
+path = '/Users/P.TEJASIVAKUMAR/PROGRAMMING/PYTHON/PYTHON_PROJECT/IMAGES'
+images = []
+classNames = []
+myList = os.listdir(path)
+
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+# def markAttendance(name):
+#     with open('/Users/P.TEJASIVAKUMAR/PROGRAMMING/PYTHON/PYTHON PROJECT/Intime.csv', 'r+') as f:
+#         myDataList = f.readlines()
+#         nameList = []
+#         for line in myDataList:
+#             entry = line.split(',')
+#             nameList.append(entry[0])
+#             if name not in nameList:
+#                 now = datetime.now()
+#                 dtString = now.strftime('%H:%M:%S')
+#                 f.writelines(f'n{name},{dtString}')
+
+x = 0
+y = 0
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+
+        matchIndex = np.argmin(faceDis)
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2),
+                          (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            x = x+1
+            y = y-1
+        else:
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2),
+                          (0, 0, 255), cv2.FILLED)
+            cv2.putText(img, "Un-known", (x1 + 6, y2 - 6),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+            if y <= 0:
+                y = y+1
+                x = x-1
+    cv2.imshow('Image Tacker', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
 
